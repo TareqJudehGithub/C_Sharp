c31636322a714de6b6204b3a60d3883904d215f1
TareqJudehGithub <tareq.joudeh@gmail.com>
2024-07-07 18:48:04 +0300

    Completed Lambda Expressions lesson and all it's related excercises.

---
+++ Lambda/LambdaExpression/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lambda Expressions\n");
+
+void GreetUser() => Console.WriteLine("Hello World!");
+
+
+GreetUser();
+
+Func<int> addition = (x, y) => x + y;
